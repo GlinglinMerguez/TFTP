d5def90d14ab967f7e7fd1157477d5ff21ecb7f2
GlinglinMerguez <leo.appourchaux@gmail.com>
2023-12-11 10:50:40 +0100

    question 4)a)

--- gettftp.c
+++ gettftp.c
@@ -2,17 +2,21 @@
 #include <string.h>
 #include <netdb.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
-#define ARGUMENTSERROR "Error, you must specify 3 arguments."
-#define SERVERERROR "Error trying to get the server adress."
+#define ARGUMENTS_ERROR "Error, you must specify 3 arguments."
+#define SERVER_ADDR_ERROR "Error trying to get the server address."
+#define SOCKET_ERROR "Socket creation error"
+#define SEND_ERROR "Error sending data"
+#define MAX_RRQ_SIZE 512
+#define RRQ_MODE "octet"
 
 void main(int argc, char** argv) {
 
     // Argument count test
     if (argc != 4) {
-        printf(ARGUMENTSERROR);
+        printf(ARGUMENTS_ERROR);
     } else {
 
         // Get the server address
@@ -23,14 +27,28 @@ void main(int argc, char** argv) {
 
         // Try to get the server address
         if (getaddrinfo(argv[1], NULL, &hints, &res) !=0) {
-            perror(SERVERERROR);
+            perror(SERVER_ADDR_ERROR);
             exit(EXIT_FAILURE);
         }
 
         // Socket Reservation
         int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
         if (sockfd == -1) {
-            perror("Socket creation error");
+            perror(SOCKET_ERROR);
+            exit(EXIT_FAILURE);
+        }
+
+        // Build RRQ packet
+        char rrq_buffer[MAX_RRQ_SIZE];
+        rrq_buffer[0] = 0x00;
+        rrq_buffer[1] = 0x01;
+        strcpy(rrq_buffer + 2, argv[2]);
+        strcpy(rrq_buffer + 2 + strlen(argv[2]) + 1, RRQ_MODE);
+        int rrq_length = 2 + strlen(rrq_buffer) + 1 + strlen(argv[2]) + 1;
+
+        // Send the RRQ packet
+        if (sendto(sockfd, rrq_buffer, rrq_length, 0, res->ai_addr, res->ai_addrlen) == -1) {
+            perror(SEND_ERROR);
             exit(EXIT_FAILURE);
         }
 
